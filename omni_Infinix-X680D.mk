c2136649318687e54edd26da2d0181e3247f3125
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-01-24 08:35:59 +0100

    Infinix-X680D: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: infinix Model: Infinix X680D Script version: 3.0.0

---
+++ omni_Infinix-X680D.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from Infinix-X680D device
+$(call inherit-product, device/infinix/Infinix-X680D/device.mk)
+
+PRODUCT_DEVICE := Infinix-X680D
+PRODUCT_NAME := omni_Infinix-X680D
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X680D
+PRODUCT_MANUFACTURER := infinix
+
+PRODUCT_GMS_CLIENTID_BASE := android-transsion-infinix-rev1
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_x680d_h6116-user 10 QP1A.190711.020 199789 release-keys"
+
+BUILD_FINGERPRINT := Infinix/X680D-IN/Infinix-X680D:10/QP1A.190711.020/J-AS-211130V404:user/release-keys
