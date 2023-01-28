7488ccb4af1e200c263ccf8ac15a9f00f857bc6f
psdutta426 <59450090+psdutta426@users.noreply.github.com>
2023-01-28 10:46:10 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_Infinix-X680D.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_Infinix-X680D-user \
-    twrp_Infinix-X680D-userdebug \
-    twrp_Infinix-X680D-eng
+    twrp_Infinix_X680D-user \
+    twrp_Infinix_X680D-userdebug \
+    twrp_Infinix_X680D-eng
